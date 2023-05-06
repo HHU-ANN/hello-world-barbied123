7360153291a955806181e4a52c33258333b2f57f
barbied123 <101971342+barbied123@users.noreply.github.com>
2023-05-06 20:16:34 +0800

    Create Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -8,9 +8,9 @@ except ImportError as e:
     os.system("sudo pip3 install numpy")
     import numpy as np
 
-def ridge(data,a=0.001):
+def ridge(data,a=0.2):
     x,y=read_data()
-    weight=np.matmul(np.linalg.inv(np.matmul(x.T,x)+aI),np.matmul(x.T,y))
+    weight=np.matmul(np.linalg.inv(np.matmul(x.T,x)+np.eye(x.shape[1]*a)),np.matmul(x.T,y))
     return weight@data
     
 def lasso(data):
